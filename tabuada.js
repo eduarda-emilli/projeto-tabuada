ca1543199b01feef74180dc7ee912e1ca6212b56
Eduarda Emilli <107655524+eduarda-emilli@users.noreply.github.com>
2022-12-03 16:24:07 -0300

    att

---
+++ tabuada.js
@@ -0,0 +1,5 @@
+var valor = document.querySelector('div.container input')
+var tabuada = document.querySelector('div.container div.tabuada')
+const calcular = document.querySelector('div.container button')
+
+calcular
